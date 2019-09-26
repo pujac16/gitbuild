55ed44d888886ecd73897522539096deccfce5a7
IN007869 <IN007869@10.172.33.8>
2019-09-26 04:09:53 -0700

    commit done by puja

---
+++ gitbuilddemo/src/gitbuildtest/printdemo.java
@@ -0,0 +1,12 @@
+ package gitbuildtest;
+
+public class printdemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("i love doreamon");
+
+		System.out.println("i love doreamon and shinchan");
+	}
+
+}
